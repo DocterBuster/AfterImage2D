879fb15544eb9f4a4371db40ad0371ce832919e7
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2026-07-28 15:37:33 -0400

    Setup for promo

--- Debug/character_body_2d.gd
+++ Debug/character_body_2d.gd
@@ -5,23 +5,32 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 
 
+@onready var sprite : Sprite2D = $Sprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		sprite.flip_h = sign(velocity.x) == -1
+		if(is_on_floor()):
+			$AnimationPlayer.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimationPlayer.play("jump")
+	
+	
 	move_and_slide()
 
 
