83674d48255e793500aa82c365dcbecf0da7e36a
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2026-08-01 16:35:50 -0400

    Up/Down Gravity (for fun)

--- Debug/character_body_2d.gd
+++ Debug/character_body_2d.gd
@@ -11,39 +11,47 @@ const MAX_SPEED = 500.0
 
 @onready var sprite : Sprite2D = $Sprite2D
 
-
-var calc_velocity : Vector2 = Vector2.ZERO
+## Velcoty math members
+var calc_velocity_to_add : Vector2 = Vector2.ZERO
+var prev_grav : Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	
-	calc_velocity = velocity.rotated(-rotation)
+	## Change up direction and rotation based on gravity force
+	if(prev_grav != get_gravity() and prev_grav != Vector2.ZERO):
+		up_direction = Vector2(sign(get_gravity()).x, -sign(get_gravity()).y)
+		var tween : Tween = create_tween()
+		tween.tween_property(self, "rotation", Vector2.UP.angle_to(up_direction), 0.2)
+	prev_grav = get_gravity()
+	
+	
+	
+	calc_velocity_to_add = Vector2.ZERO
 	
 	# Add the gravity.
 	if not is_on_floor():
-		calc_velocity += get_gravity() * delta
+		velocity += get_gravity() * delta
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		calc_velocity.x += direction * SPEED
-		calc_velocity.x = clampf(calc_velocity.x, -MAX_SPEED, MAX_SPEED)
-		sprite.flip_h = sign(calc_velocity.x) == -1
+		velocity.x += direction * SPEED
+		velocity.x = clampf(velocity.x, -MAX_SPEED, MAX_SPEED)
+		sprite.flip_h = sign(velocity.x) == -1
 		if(is_on_floor()):
 			$AnimationPlayer.play("walk")
 	else:
-		calc_velocity.x = move_toward(calc_velocity.x, 0, SPEED * SLOWDOWN_RATIO)
+		velocity.x = move_toward(velocity.x, 0, SPEED * SLOWDOWN_RATIO)
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		calc_velocity.y += JUMP_VELOCITY
+		velocity.y += JUMP_VELOCITY * -sign(up_direction.y)
 		$AnimationPlayer.play("jump")
 	
 	## Rotate velcoty based on current rotation & add it!
-	calc_velocity = calc_velocity.rotated(rotation)
-	velocity = calc_velocity
-
+	#velocity = velocity.rotated(rotation)
 	
 	move_and_slide()
 
