fc586cf135626f3f87b6c157564bfa8977d6cc07
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2026-08-04 21:06:46 -0400

    Sphere Physics

--- Debug/character_body_2d.gd
+++ Debug/character_body_2d.gd
@@ -18,12 +18,15 @@ var prev_grav : Vector2 = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	
 	## Change up direction and rotation based on gravity force
-	if(prev_grav != get_gravity() and prev_grav != Vector2.ZERO):
-		up_direction = Vector2(sign(get_gravity()).x, -sign(get_gravity()).y)
-		var tween : Tween = create_tween()
-		tween.tween_property(self, "rotation", Vector2.UP.angle_to(up_direction), 0.2)
-	prev_grav = get_gravity()
+	#if(prev_grav != get_gravity() and prev_grav != Vector2.ZERO):
+	up_direction = Vector2(-get_gravity().x, -get_gravity().y)
+	var tween : Tween = create_tween()
+	tween.tween_property(self, "rotation", Vector2.UP.angle_to(up_direction), 0.2)
+
 	
+
+	#up_direction = Vector2(-get_gravity().x, -get_gravity().y)
+	#rotation = Vector2.UP.angle_to(up_direction)
 	
 	
 	#calc_velocity_to_add = Vector2.ZERO
@@ -36,6 +39,11 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
+	direction = direction.rotated(rotation)
+	
+	## Calc velocity to be relative first
+	velocity = velocity.rotated(rotation)
+	
 	if direction.x:
 		velocity.x += direction.x * SPEED
 		velocity.x = clampf(velocity.x, -MAX_SPEED, MAX_SPEED)
@@ -47,8 +55,11 @@ func _physics_process(delta: float) -> void:
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y += JUMP_VELOCITY * -sign(up_direction.y)
+		velocity.y += JUMP_VELOCITY
 		$AnimationPlayer.play("jump")
+	
+	## Unrotate the vector
+	velocity = velocity.rotated(-rotation)
 	
 	move_and_slide()
 
